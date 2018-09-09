c450715ed60adaf4401eb810067db699d8a47123
Vadim1707 <43015952+Vadim1707@users.noreply.github.com>
2018-09-09 14:50:11 +0300

    Update rand1.cpp

--- rand1.cpp
+++ rand1.cpp
@@ -4,10 +4,10 @@
 
 const int m=9091, a=70, c=1067;
 int arr_saver[10]={};
-int x=time(0)%m;
+int x=20*time(0)%m;
 int IT=100000;
 
-void work_func(){ //IT iterations
+void randomize(){ //IT iterations
 for (int i=0; i<IT; i++){
     x=(a*x+c)%m;
     for(int j=1; j<=10; j++){
@@ -16,7 +16,7 @@ if ( (x*1.0/m)<0.1*j ) {arr_saver[j-1]++; break;}
 }
 }
 
-void out_func(){
+void show_results_in_percent(){
 
 for(int j=0; j<10; j++){
 std::cout<<"["<<0.1*j<<";"<<0.1*(j+1)<<")   "<<arr_saver[j]*1.0/IT<<"%\n";
